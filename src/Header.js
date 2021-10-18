7533b672b1b44fc2074b0c9e9436f9f1fd0508b8
annonomans <infotonoman@gmail.com>
2021-10-18 22:51:51 +0530

    setup

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import './App.css';
+// import { Route, Switch, Link } from "react-router-dom";
+
+class Header extends Component {
+
+	render() {
+        const site_title = "annonomans"
+    	return (	
+
+            <div>
+                <h2>Hello , {site_title}</h2>
+                <nav>
+                    <ul id="navigation">
+                        <li>
+                        <h3>Home</h3>
+                        </li>
+                        <li>
+                        <h3>About</h3>
+                        </li>
+                        <li>
+                        <h3>Contact</h3>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+    		
+    		);
+  	}
+}
+
+export default Header;
